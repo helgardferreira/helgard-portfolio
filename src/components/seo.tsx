f0f634d6541dde6d315bbf7a7b9b6eb78a50ab39
Helgard Richard Ferreira <hrfer3@gmail.com>
2020-11-10 12:21:16 +0200

    Add Lato font

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -98,7 +98,11 @@ const SEO: FunctionComponent<IProps> = ({
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;700&display=swap"
         rel="stylesheet"
-      ></link>
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
+        rel="stylesheet"
+      />
     </Helmet>
   )
 }
