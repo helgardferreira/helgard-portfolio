31f33798f93d7fc49a5adc868499ac5ace2687a2
Helgard Richard Ferreira <hrfer3@gmail.com>
2020-11-12 10:10:43 +0200

    Modify infinite scroll wording

--- src/components/backgroundScroll/index.tsx
+++ src/components/backgroundScroll/index.tsx
@@ -4,11 +4,15 @@ import InfiniteTextScroll from "./infiniteTextScroll"
 const BackgroundScroll = () => {
   return (
     <div>
-      <InfiniteTextScroll width={2200} height="500px">
-        creative
+      {/* <InfiniteTextScroll width={2200} height="500px"> */}
+      <InfiniteTextScroll width={2000} height="500px">
+        {/* creative */}
+        helgard
       </InfiniteTextScroll>
-      <InfiniteTextScroll direction="left" width={3000} height="500px">
-        intelligence
+      <InfiniteTextScroll direction="left" width={2000} height="500px">
+        {/* <InfiniteTextScroll direction="left" width={3000} height="500px"> */}
+        {/* intelligence */}
+        ferreira
       </InfiniteTextScroll>
     </div>
   )
