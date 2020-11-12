dd363baab4f5f9bd6a3adf8d328c6ee43c9b5568
Helgard Richard Ferreira <hrfer3@gmail.com>
2020-11-12 07:38:11 +0200

    Add Raleway font for GUI

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -103,6 +103,11 @@ const SEO: FunctionComponent<SeoProps> = ({
         href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
         rel="stylesheet"
       />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
     </Helmet>
   )
 }
